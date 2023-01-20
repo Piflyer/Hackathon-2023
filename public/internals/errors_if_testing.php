ea3db29c18fbc74810bc10e44d9269c7e761b2d9
Sidney Trzepacz <sidtrzisyourleader@gmail.com>
2023-01-20 18:50:13 -0500

    testing

--- public/internals/errors_if_testing.php
+++ public/internals/errors_if_testing.php
@@ -1,6 +1,9 @@
 <?php
-if(str_contains($_SERVER['SERVER_NAME'], "PhpStorm")) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-}
+//if(str_contains($_SERVER['SERVER_NAME'], "PhpStorm")) {
+//    ini_set('display_errors', 1);
+//    ini_set('display_startup_errors', 1);
+//    error_reporting(E_ALL);
+//}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
