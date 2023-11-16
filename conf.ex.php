33f9fa51e44a3254f16c89f68169cd9b316110a9
sid <sidneytrzepacz@gmail.com>
2023-11-16 16:32:26 -0500

    Example conf

--- conf.ex.php
+++ conf.ex.php
@@ -1,1 +1,9 @@
 <?php
+global $config;
+$config = [
+    "DATABASE_SERVER" => "localhost",
+    "DATABASE_USER" => "root",
+    "DATABASE_PASS" => "",
+    "DATABASE_NAME" => "metaverse",
+    "NODE_SERVER" => "https://google.com"
+];
